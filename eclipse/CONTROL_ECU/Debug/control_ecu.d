66a1990b1ce45977f8e68688d47f9798ae91076e
FatmaAshraf76 <143643641+FatmaAshraf76@users.noreply.github.com>
2024-11-23 16:05:23 +0200

    Add files via upload

---
+++ eclipse/CONTROL_ECU/Debug/control_ecu.d
@@ -0,0 +1,23 @@
+control_ecu.o control_ecu.o: ../control_ecu.c ../uart.h ../std_types.h \
+  ../motor.h ../timer.h ../buzzer.h ../pwm.h ../twi.h ../pir.h \
+  ../external_eeprom.h ../gpio.h
+
+../uart.h:
+
+../std_types.h:
+
+../motor.h:
+
+../timer.h:
+
+../buzzer.h:
+
+../pwm.h:
+
+../twi.h:
+
+../pir.h:
+
+../external_eeprom.h:
+
+../gpio.h:
---
+++ eclipse/CONTROL_ECU/Debug/timer.d
@@ -0,0 +1,10 @@
+timer.o timer.o: ../timer.c ../common_macros.h ../timer.h ../std_types.h \
+  ../gpio.h
+
+../common_macros.h:
+
+../timer.h:
+
+../std_types.h:
+
+../gpio.h:
